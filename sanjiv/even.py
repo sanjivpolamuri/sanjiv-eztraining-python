b4b233e31974010e4afe9eebefe3a31621847f0e
sanjivpolamuri <124117805+sanjivpolamuri@users.noreply.github.com>
2023-02-02 09:27:28 +0530

    Add files via upload

---
+++ sanjiv/even.py
@@ -0,0 +1,8 @@
+l=[]
+len=6
+for i in range(0,len):
+    num=int(input())
+    l.append(num)
+for j in l:
+    if j%2==0:
+        print(j,end=" ")
---
+++ sanjiv/l.py
@@ -0,0 +1,3 @@
+l=[1,2,3,4,5,"ichigo","bankai","bakuya",7.8,9.9]
+for i in l:
+    print(i)
