22db8d6ca1b4215c82717bd87c1b175beff26bb5
P V Sanjiv <124117805+sanjivpolamuri@users.noreply.github.com>
2024-06-21 15:45:05 +0530

    Update passwordgenerator.py

--- passwordgenerator.py
+++ passwordgenerator.py
@@ -4,5 +4,7 @@ lower = "abcdefghijklmnopqrstvwxyz"
 upper = "ABCDEFGHIJKLMNOPQRSTVWXYZ"
 numbers = "0123456789"
 symbols = "!@#$%^&*()_-+=?>< ["
-all_chars = lower + upper + numbers + symbols length = int(input("Enter a length: "))
-password = ''.join(random.sample (all_chars, length)) print("Generated Password: ", password)
+all_chars = lower + upper + numbers + symbols 
+length = int(input("Enter a length: "))
+password = ''.join(random.sample (all_chars, length)) 
+print("Generated Password: ", password)
