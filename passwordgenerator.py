2096fe58e8015a4a292c6f345b53d9d4a9418c7f
P V Sanjiv <124117805+sanjivpolamuri@users.noreply.github.com>
2024-06-21 15:44:22 +0530

    Add files via upload

---
+++ passwordgenerator.py
@@ -0,0 +1,8 @@
+import random
+
+lower = "abcdefghijklmnopqrstvwxyz"
+upper = "ABCDEFGHIJKLMNOPQRSTVWXYZ"
+numbers = "0123456789"
+symbols = "!@#$%^&*()_-+=?>< ["
+all_chars = lower + upper + numbers + symbols length = int(input("Enter a length: "))
+password = ''.join(random.sample (all_chars, length)) print("Generated Password: ", password)
